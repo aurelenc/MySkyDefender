affb52f8719ad435a810b46255144595501fa0ae
aurelenc <aurele.nicolas@epitech.eu>
2022-08-05 22:39:00 +0200

    Add new Idea project

---
+++ src/main/java/fr/luckykous/myskydefender/MySkyDefender.java
@@ -0,0 +1,17 @@
+package fr.luckykous.myskydefender;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MySkyDefender extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
